7098b5c55fa794572a95eec0d96a9edad0709547
Ralph Lawrence Silaya <rrsilaya@gmail.com>
2025-03-02 12:42:41 +0800

    Request to appscript

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from soa2sheets.lib.bpi.utils import Date
 from flask import Request, jsonify
 from pypdf import PdfReader
 
+import requests
+
 def parse_soa(request: Request):
   if request.method != 'POST':
     return jsonify({ 'error': 'Method not allowed' }), 405
@@ -11,6 +13,10 @@ def parse_soa(request: Request):
   file = request.files['file']
   if not file:
     return jsonify({ 'error': 'No file uploaded' }), 400
+  
+  webapp_id = request.form.get('webapp_id')
+  if not webapp_id:
+    return jsonify({ 'error': 'No webapp ID provided' }), 400
 
   pdf = PdfReader(file)
   text = ''.join([page.extract_text() for page in pdf.pages])
@@ -18,7 +24,7 @@ def parse_soa(request: Request):
   statement_info = BpiStatementInfo.parse(text)
   transactions = BpiTransaction.parse_multiple(text, year=statement_info.statement_date.year)
 
-  return jsonify({
+  payload = {
     'account_info': {
       'customer_number': statement_info.customer_number,
       'credit_limit': statement_info.credit_limit,
@@ -31,4 +37,10 @@ def parse_soa(request: Request):
       'minimum_due': statement_info.minimum_due,
     },
     'transactions': [transaction.to_json() for transaction in transactions],
-  })
+  }
+
+  webapp_id = request.form.get('webapp_id')
+  req = requests.post(f'https://script.google.com/macros/s/{webapp_id}/exec', json=payload)
+  response = req.json()
+
+  return jsonify({**payload, 'response': response})
